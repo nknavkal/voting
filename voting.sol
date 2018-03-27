ebe582a4c35be56c603ab1691fbd30d4c8969261
Nishaad Navkal <nknavkal@gmail.com>
2018-03-27 16:31:18 +0200

    voting.sol matches

---
+++ voting.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.11;
+
+contract Voting {
+
+  mapping (bytes32 => uint8) public votesReceived;
+
+  bytes32[] public candidateList;
+
+  function Voting(bytes32[] candidateNames) public {
+    candidateList = candidateNames;
+  }
+
+  function totalVotesFor(bytes32 candidate) public view returns (uint8) {
+    assert(validCandidate(candidate));
+    return votesReceived[candidate];
+  }
+
+  function voteForCandidate(bytes32 candidate) public {
+    assert(validCandidate(candidate));
+    votesReceived[candidate] += 1;
+  }
+
+  function validCandidate(bytes32 candidate) public view returns (bool) {
+    for(uint i = 0; i < candidateList.length; i += 1) {
+      if(candidateList[i] == candidate) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
