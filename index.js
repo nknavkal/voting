9f8cd348af07214f14f1de74a72dad35a07766f8
Nishaad Navkal <nknavkal@gmail.com>
2018-03-27 16:09:21 +0200

    start of this tutorial

---
+++ index.js
@@ -0,0 +1,10 @@
+prama solidity ^0.4.11;
+
+contract voting {
+
+  mapping(bytes32 ==> uint8) public votesReceived;
+
+  bytes32[] public candidateList;
+
+  function voting(bytes32[] candidateNames)
+}
